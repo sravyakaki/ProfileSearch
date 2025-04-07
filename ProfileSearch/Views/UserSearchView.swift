4eb4c65dc493b8fdce1c784eaf76bc6cfe19da47
SKaki <sravyakaki@Sravyas-Mac-mini.local>
2025-04-07 12:25:22 -0700

    Asset and folders setup

---
+++ ProfileSearch/Views/UserSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  UserSearchView.swift
+//  ProfileSearch
+//
+//  Created by Sravya Kaki on 2025-04-07.
+//
+
+import SwiftUI
+
+struct UserSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSearchView()
+}
