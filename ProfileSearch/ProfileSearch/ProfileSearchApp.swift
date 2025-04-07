b99b3b4c63b676fc19fa460a1a1c7c4d2d836796
SKaki <sravyakaki@Sravyas-Mac-mini.local>
2025-04-07 11:12:03 -0700

    Setup Files

---
+++ ProfileSearch/ProfileSearch/ProfileSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileSearchApp.swift
+//  ProfileSearch
+//
+//  Created by Sravya Kaki on 2025-04-07.
+//
+
+import SwiftUI
+
+@main
+struct ProfileSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
